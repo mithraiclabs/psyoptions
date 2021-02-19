7d681018ac2dd8fa20e52e82964b7d4b9ad280f7
Taylor123 <cjohns28@terpmail.umd.edu>
2021-02-18 20:03:24 -0800

    set up eslint with ts

--- packages/js-bindings/src/index.ts
+++ packages/js-bindings/src/index.ts
@@ -1,1 +1,1 @@
-export * from './initializeMarket'
+export * from './initializeMarket';
--- packages/js-bindings/src/layout.ts
+++ packages/js-bindings/src/layout.ts
@@ -3,13 +3,9 @@ import * as BufferLayout from 'buffer-layout';
 /**
  * Layout for a public key
  */
-export const publicKey = (property: string) => {
-  return BufferLayout.blob(32, property);
-};
+export const publicKey = (property: string) => BufferLayout.blob(32, property);
 
 /**
  * Layout for a 64bit unsigned value
  */
-export const uint64 = (property: string) => {
-  return BufferLayout.blob(8, property);
-};
+export const uint64 = (property: string) => BufferLayout.blob(8, property);
