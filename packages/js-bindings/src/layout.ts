8d10515fb993353595f092bb5638b0403a7aca42
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-02-18 19:56:43 -0800

    add buffer layout declaration

--- packages/js-bindings/src/layout.ts
+++ packages/js-bindings/src/layout.ts
@@ -3,13 +3,13 @@ import * as BufferLayout from 'buffer-layout';
 /**
  * Layout for a public key
  */
-export const publicKey = (property) => {
+export const publicKey = (property: string) => {
   return BufferLayout.blob(32, property);
 };
 
 /**
  * Layout for a 64bit unsigned value
  */
-export const uint64 = (property) => {
+export const uint64 = (property: string) => {
   return BufferLayout.blob(8, property);
 };
