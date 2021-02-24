63e7002a6f07dffb0b9af7921353e178d0e73ccd
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-02-23 18:38:02 -0800

    Add Market class and ability to query all OptionMarkets

--- packages/js-bindings/src/market.ts
+++ packages/js-bindings/src/market.ts
@@ -1,3 +1,9 @@
+import {
+  AccountInfo,
+  Connection,
+  PublicKey,
+  RpcResponseAndContext,
+} from '@solana/web3.js';
 import * as BufferLayout from 'buffer-layout';
 import * as Layout from './layout';
 
@@ -7,7 +13,7 @@ export type OptionWriter = {
   underlyingAssetAcctAddress: string;
   quoteAssetAcctAddress: string;
   contractTokenAcctAddress: string;
-}
+};
 export const optionWriterStructArray = [
   Layout.publicKey('underlyingAssetAcctAddress'),
   Layout.publicKey('quoteAssetAcctAddress'),
@@ -40,3 +46,76 @@ export const OPTION_MARKET_LAYOUT = BufferLayout.struct([
   BufferLayout.u16('registryLength'),
   BufferLayout.seq(OPTION_WRITER_LAYOUT, MAX_CONTRACTS, 'optionWriterRegistry'),
 ]);
+
+type SolanaRpcResponse = {
+  pubkey: string;
+  account: {
+    data: string;
+    executable: boolean;
+    owner: string;
+    lamports: string;
+  };
+};
+
+export class Market {
+  programId: PublicKey;
+
+  pubkey: PublicKey;
+
+  marketData: OptionMarket;
+
+  constructor(programId: PublicKey, pubkey: PublicKey, accountData: Buffer) {
+    this.programId = programId;
+    this.pubkey = pubkey;
+    this.marketData = OPTION_MARKET_LAYOUT.decode(accountData) as OptionMarket;
+  }
+
+  static getAllMarkets = async (
+    connection: Connection,
+    programId: PublicKey,
+  ) => {
+    const res = await Market.getFilteredProgramAccounts(
+      connection,
+      programId,
+      [],
+    );
+    return res.map(
+      // eslint-disable-next-line prettier/prettier
+      ({ publicKey, accountInfo }) => new Market(programId, publicKey, accountInfo.data),
+    );
+  };
+
+  static getFilteredProgramAccounts = async (
+    connection: Connection,
+    programId: PublicKey,
+    filters: any,
+  ): Promise<{ publicKey: PublicKey; accountInfo: AccountInfo<Buffer> }[]> => {
+    // @ts-ignore
+    // eslint-disable-next-line no-underscore-dangle
+    const resp = await connection._rpcRequest('getProgramAccounts', [
+      programId.toBase58(),
+      {
+        commitment: connection.commitment,
+        filters,
+        encoding: 'base64',
+      },
+    ]);
+    if (resp.error) {
+      throw new Error(resp.error.message);
+    }
+    return resp.result.map(
+      ({
+        pubkey,
+        account: { data, executable, owner, lamports },
+      }: SolanaRpcResponse) => ({
+        publicKey: new PublicKey(pubkey),
+        accountInfo: {
+          data: Buffer.from(data[0], 'base64'),
+          executable,
+          owner: new PublicKey(owner),
+          lamports,
+        },
+      }),
+    );
+  };
+}
