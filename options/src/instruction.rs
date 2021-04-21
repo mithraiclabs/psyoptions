763ab84cc305d3f5d12af56374f35ab4d36f0a8a
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-04-20 21:20:16 -0700

    move declared fee owner to fees module

--- options/src/instruction.rs
+++ options/src/instruction.rs
@@ -9,14 +9,6 @@ use solana_program::{
 use spl_token;
 use std::mem::size_of;
 
-/// The fee_owner_key will own all of the associated accounts where token fees are paid to.
-/// In the future this should be a program derived address owned by a fully decntralized 
-/// fee sweeping program.
-pub mod fee_owner_key {
-    use solana_program::declare_id;
-    declare_id!("7XbrrKfaoEbdSXksZ98ST1Wv6gATVAvFGcZEvxhdKAt2");
-}
-
 /// Instructions supported by the Options program
 #[repr(C)]
 #[derive(Debug, PartialEq)]
