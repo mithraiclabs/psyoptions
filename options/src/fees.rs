e4ef84691196fe9c927c0b8b009de4d05ff212be
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-04-21 07:03:49 -0700

    WIP security

--- options/src/fees.rs
+++ options/src/fees.rs
@@ -1,5 +1,11 @@
-use solana_program::pubkey::Pubkey;
+use solana_program::{
+  account_info::AccountInfo,
+  program_error::ProgramError,
+  pubkey::Pubkey,
+  program_pack::Pack,
+};
 use spl_associated_token_account::get_associated_token_address;
+use spl_token::state::Account;
 
 /// The fee_owner_key will own all of the associated accounts where token fees are paid to.
 /// In the future this should be a program derived address owned by a fully decntralized
@@ -16,7 +22,17 @@ pub mod fee_owner_key {
 /// 4. If not initialized, call cross program invocation to `create_associated_token_account` to
 /// initialize
 /// 5. Return the fee accounts public key
-pub fn get_or_create_fee_account(mint: Pubkey) {
+pub fn get_or_create_fee_account(mint: Pubkey, fee_account: AccountInfo) -> Result<(), ProgramError> {
   let _account_address = get_associated_token_address(&fee_owner_key::ID, &mint);
-
+  let account_data = fee_account.try_borrow_data()?;
+  let token_account = Account::unpack(&account_data)?;
+  // check that the account's owner is
+  if token_account.owner != fee_owner_key::ID {
+    // TODO return some error
+  }
+  if token_account.mint != mint {
+    // TODO return some error
+  }
+  
+  Ok(())
 }
