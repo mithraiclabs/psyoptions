dfc2d371bc050d6328364c865f819480482017eb
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-04-22 20:20:39 -0700

    finish and rename to validate_fee_account

--- options/src/fees.rs
+++ options/src/fees.rs
@@ -1,10 +1,11 @@
 use solana_program::{
   account_info::AccountInfo,
+  program::invoke,
   program_error::ProgramError,
+  program_pack::{IsInitialized, Pack},
   pubkey::Pubkey,
-  program_pack::Pack,
 };
-use spl_associated_token_account::get_associated_token_address;
+use spl_associated_token_account::{create_associated_token_account, get_associated_token_address};
 use spl_token::state::Account;
 
 /// The fee_owner_key will own all of the associated accounts where token fees are paid to.
@@ -22,17 +23,29 @@ pub mod fee_owner_key {
 /// 4. If not initialized, call cross program invocation to `create_associated_token_account` to
 /// initialize
 /// 5. Return the fee accounts public key
-pub fn get_or_create_fee_account(mint: Pubkey, fee_account: AccountInfo) -> Result<(), ProgramError> {
+pub fn validate_fee_account<'a>(
+  mint: Pubkey,
+  fee_owner_account: AccountInfo,
+  fee_account: AccountInfo<'a>,
+) -> Result<AccountInfo<'a>, ProgramError> {
   let _account_address = get_associated_token_address(&fee_owner_key::ID, &mint);
-  let account_data = fee_account.try_borrow_data()?;
-  let token_account = Account::unpack(&account_data)?;
-  // check that the account's owner is
+  let token_account = {
+    let account_data = fee_account.try_borrow_data()?;
+    Account::unpack(&account_data)?
+  };
+  // check that the account's owner is the fee owner
   if token_account.owner != fee_owner_key::ID {
-    // TODO return some error
+    return Err(ProgramError::InvalidAccountData);
   }
+  // check that the mint is the correct fee mint
   if token_account.mint != mint {
-    // TODO return some error
+    return Err(ProgramError::InvalidAccountData);
   }
-  
-  Ok(())
+  // if the token account is not initialized create it
+  if !token_account.is_initialized() {
+    let create_account_ix =
+      create_associated_token_account(&fee_owner_key::ID, &fee_owner_key::ID, &mint);
+    invoke(&create_account_ix, &[fee_owner_account.clone()])?;
+  }
+  Ok(fee_account)
 }
