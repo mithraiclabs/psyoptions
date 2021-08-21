444298fdfb45efc1b44a0cc2f3c81140f635a44a
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-08-21 09:10:23 -0700

    check that the asset amounts are > 0

--- programs/psy_american/src/errors.rs
+++ programs/psy_american/src/errors.rs
@@ -4,4 +4,6 @@ use anchor_lang::prelude::*;
 pub enum PsyOptionsError {
   #[msg("Same quote and underlying asset, cannot create market")]
   QuoteAndUnderlyingAssetMustDiffer,
+  #[msg("Quote amount and underlying amount per contract must be > 0")]
+  QuoteOrUnderlyingAmountCannotBe0,
 }
