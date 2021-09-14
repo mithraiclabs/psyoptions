83e89996b7c0188993e42e09babe1873364fdaae
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-09-14 15:49:07 -0700

    add test for proxying an order

--- tests/serum/initOpenOrders.ts
+++ tests/serum/initOpenOrders.ts
@@ -21,11 +21,16 @@ describe("initOpenOrders", () => {
   const program = anchor.workspace.PsyAmerican as anchor.Program;
   // Token client.
   let usdcClient;
-  let usdcMint: Keypair;
 
   // Global DEX accounts and clients shared across all tests.
-  let marketProxy: MarketProxy, tokenAccount, usdcAccount;
-  let openOrdersBump, openOrdersInitAuthority, openOrdersBumpinit;
+  let marketProxy: MarketProxy,
+    tokenAccount,
+    usdcMint: Keypair,
+    usdcAccount: PublicKey;
+  let openOrdersKey: PublicKey,
+    openOrdersBump: number,
+    openOrdersInitAuthority,
+    openOrdersBumpinit;
   let usdcPosted;
   let referralTokenAddress;
   // Global PsyOptions accounts
@@ -57,7 +62,7 @@ describe("initOpenOrders", () => {
       remainingAccounts,
       instructions
     );
-    ({ marketA: marketProxy } = await initMarket(
+    ({ marketA: marketProxy, godUsdc: usdcAccount } = await initMarket(
       provider,
       program,
       marketLoader(provider, program),
@@ -70,7 +75,7 @@ describe("initOpenOrders", () => {
     const tx = new Transaction();
     const dummy = new Keypair();
     const owner = program.provider.wallet.publicKey;
-    const [openOrdersKey, bump] = await PublicKey.findProgramAddress(
+    [openOrdersKey, openOrdersBump] = await PublicKey.findProgramAddress(
       [
         openOrdersSeed,
         DEX_PID.toBuffer(),
@@ -97,5 +102,55 @@ describe("initOpenOrders", () => {
     const decoded = OpenOrders.getLayout(DEX_PID).decode(account.data);
     const openOrders = new OpenOrders(openOrdersKey, decoded, DEX_PID);
     assert.equal(openOrders.owner.toString(), openOrdersKey.toString());
+  });
+
+  it("Posts a bid on the orderbook", async () => {
+    const size = 1;
+    const price = 1;
+    usdcPosted = new anchor.BN(
+      // @ts-ignore
+      marketProxy.market._decoded.quoteLotSize.toNumber()
+    ).mul(
+      marketProxy.market
+        .baseSizeNumberToLots(size)
+        .mul(marketProxy.market.priceNumberToLots(price))
+    );
+    let openOrders = OpenOrders.load(
+      provider.connection,
+      openOrdersKey,
+      DEX_PID
+    );
+    let orders = (await openOrders).orders;
+    assert.equal(orders.filter((id) => !id.isZero()).length, 0);
+
+    let bids = await marketProxy.market.loadBids(provider.connection);
+    let l2 = await bids.getL2(2);
+    assert.equal(l2.length, 0);
+
+    const tx = new Transaction();
+    tx.add(
+      marketProxy.instruction.newOrderV3({
+        owner: program.provider.wallet.publicKey,
+        payer: usdcAccount,
+        side: "buy",
+        price,
+        size,
+        orderType: "postOnly",
+        clientId: new anchor.BN(999),
+        openOrdersAddressKey: openOrdersKey,
+        selfTradeBehavior: "abortTransaction",
+      })
+    );
+    await provider.send(tx);
+
+    // TODO: Validate that the new order is in the OpenOrders
+    openOrders = OpenOrders.load(provider.connection, openOrdersKey, DEX_PID);
+    orders = (await openOrders).orders;
+    assert.equal(orders.filter((id) => !id.isZero()).length, 1);
+
+    bids = await marketProxy.market.loadBids(provider.connection);
+    const [p, s] = await bids.getL2(2)[0];
+    assert.equal(price, p);
+    assert.equal(size, s);
   });
 });
