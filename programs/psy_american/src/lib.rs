60ab9aab208f531566925d2ddf000b4265251ec0
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-12-07 19:54:41 -0800

    refactor for mint V2

--- programs/psy_american/src/lib.rs
+++ programs/psy_american/src/lib.rs
@@ -3,7 +3,7 @@ pub mod fees;
 pub mod serum_proxy;
 
 use anchor_lang::{AccountsExit, Key, prelude::*};
-use anchor_spl::token::{self, Burn, Mint, MintTo, TokenAccount, Transfer};
+use anchor_spl::token::{self, Burn, Mint, MintTo, TokenAccount, Transfer, Token};
 use spl_token::state::Account as SPLTokenAccount;
 use solana_program::{program::invoke, program_error::ProgramError, program_pack::Pack, system_instruction, system_program};
 use serum_dex::instruction::{initialize_market as init_serum_market_instruction};
@@ -139,6 +139,54 @@ pub mod psy_american {
         };
         let cpi_token_program = ctx.accounts.token_program.clone();
         let cpi_ctx = CpiContext::new_with_signer(cpi_token_program, cpi_accounts, signer);
+        token::mint_to(cpi_ctx, size)?;
+
+        Ok(())
+    }
+
+    #[access_control(MintOptionV2::unexpired_market(&ctx) MintOptionV2::accounts(&ctx) validate_size(size))]
+    pub fn mint_option_v2<'a, 'b, 'c, 'info>(ctx: Context<'a, 'b, 'c, 'info, MintOptionV2<'info>>, size: u64) -> ProgramResult {
+        let option_market = &ctx.accounts.option_market;
+
+        // Transfer the underlying assets to the underlying assets pool
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.underlying_asset_src.to_account_info(),
+            to: ctx.accounts.underlying_asset_pool.to_account_info(),
+            authority: ctx.accounts.user_authority.clone(),
+        };
+        let cpi_token_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new(cpi_token_program.to_account_info(), cpi_accounts);
+        let underlying_transfer_amount = option_market.underlying_amount_per_contract.checked_mul(size).unwrap();
+        token::transfer(cpi_ctx, underlying_transfer_amount)?;
+
+        let seeds = &[
+            option_market.underlying_asset_mint.as_ref(),
+            option_market.quote_asset_mint.as_ref(),
+            &option_market.underlying_amount_per_contract.to_le_bytes(),
+            &option_market.quote_amount_per_contract.to_le_bytes(),
+            &option_market.expiration_unix_timestamp.to_le_bytes(),
+            &[option_market.bump_seed]
+        ];
+        let signer = &[&seeds[..]];
+
+        // Mint a new OptionToken(s)
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.option_mint.to_account_info(),
+            to: ctx.accounts.minted_option_dest.to_account_info(),
+            authority: ctx.accounts.option_market.to_account_info(),
+        };
+        let cpi_token_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_token_program.to_account_info(), cpi_accounts, signer);
+        token::mint_to(cpi_ctx, size)?;
+
+        // Mint a new WriterToken(s)
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.writer_token_mint.to_account_info(),
+            to: ctx.accounts.minted_writer_token_dest.to_account_info(),
+            authority: ctx.accounts.option_market.to_account_info(),
+        };
+        let cpi_token_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_token_program.to_account_info(), cpi_accounts, signer);
         token::mint_to(cpi_ctx, size)?;
 
         Ok(())
@@ -669,6 +717,64 @@ impl<'info> MintOption<'info> {
     fn unexpired_market(ctx: &Context<MintOption<'info>>) -> Result<(), ProgramError> {
         // Validate the market is not expired
         if ctx.accounts.option_market.expiration_unix_timestamp < ctx.accounts.clock.unix_timestamp {
+            return Err(errors::ErrorCode::OptionMarketExpiredCantMint.into())
+        }
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct MintOptionV2<'info> {
+    /// The user authority must be the authority that has ownership of the `underlying_asset_src`
+    #[account(mut, signer)]
+    pub user_authority: AccountInfo<'info>,
+    pub underlying_asset_mint: AccountInfo<'info>,
+    #[account(mut)]
+    pub underlying_asset_pool: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub underlying_asset_src: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub option_mint: Box<Account<'info, Mint>>,
+    #[account(mut)]
+    pub minted_option_dest: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub writer_token_mint: Box<Account<'info, Mint>>,
+    #[account(mut)]
+    pub minted_writer_token_dest: Box<Account<'info, TokenAccount>>,
+    pub option_market: Box<Account<'info, OptionMarket>>,
+    #[account(mut)]
+    pub fee_owner: AccountInfo<'info>,
+
+
+    pub token_program: Program<'info, Token>,
+}
+impl<'info> MintOptionV2<'info> {
+    fn accounts(ctx: &Context<MintOptionV2<'info>>) -> Result<(), ProgramError> {
+        // Validate the underlying asset pool is the same as on the OptionMarket
+        if *ctx.accounts.underlying_asset_pool.to_account_info().key != ctx.accounts.option_market.underlying_asset_pool {
+            return Err(errors::ErrorCode::UnderlyingPoolAccountDoesNotMatchMarket.into())
+        }
+
+        // Validate the option mint is the same as on the OptionMarket
+        if *ctx.accounts.option_mint.to_account_info().key != ctx.accounts.option_market.option_mint {
+            return Err(errors::ErrorCode::OptionTokenMintDoesNotMatchMarket.into())
+        }
+
+        // Validate the writer token mint is the same as on the OptionMarket
+        if *ctx.accounts.writer_token_mint.to_account_info().key != ctx.accounts.option_market.writer_token_mint {
+            return Err(errors::ErrorCode::WriterTokenMintDoesNotMatchMarket.into())
+        }
+
+        // Validate the fee owner is correct
+        if *ctx.accounts.fee_owner.key != fees::fee_owner_key::ID {
+            return Err(errors::ErrorCode::FeeOwnerDoesNotMatchProgram.into())
+        }
+
+        Ok(())
+    }
+    fn unexpired_market(ctx: &Context<MintOptionV2<'info>>) -> Result<(), ProgramError> {
+        // Validate the market is not expired
+        if ctx.accounts.option_market.expiration_unix_timestamp < Clock::get()?.unix_timestamp {
             return Err(errors::ErrorCode::OptionMarketExpiredCantMint.into())
         }
         Ok(())
---
+++ tests/mint_v2.ts
@@ -0,0 +1,599 @@
+import * as anchor from "@project-serum/anchor";
+import assert from "assert";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+  u64,
+} from "@solana/spl-token";
+import {
+  AccountMeta,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import {
+  feeAmountPerContract,
+  FEE_OWNER_KEY,
+  NFT_MINT_LAMPORTS,
+} from "../packages/psyoptions-ts/src/fees";
+import {
+  createMinter,
+  initNewTokenAccount,
+  initNewTokenMint,
+  initOptionMarket,
+  initSetup,
+  wait,
+} from "../utils/helpers";
+import { OptionMarketV2 } from "../packages/psyoptions-ts/src/types";
+import { BN } from "@project-serum/anchor";
+
+describe("mintOption", () => {
+  const provider = anchor.Provider.env();
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.PsyAmerican as anchor.Program;
+
+  const minter = anchor.web3.Keypair.generate();
+
+  let quoteToken: Token;
+  let underlyingToken: Token;
+  let optionToken: Token;
+  let optionMarket: OptionMarketV2;
+  let underlyingAmountPerContract: anchor.BN;
+  let quoteAmountPerContract: anchor.BN;
+  let expiration: anchor.BN;
+  let optionMarketKey: PublicKey;
+  let bumpSeed: number;
+  let mintFeeKey: PublicKey | null;
+  let exerciseFeeKey: PublicKey;
+  let optionMintAccount: Keypair;
+  let writerTokenMintAccount: Keypair;
+  let underlyingAssetPoolAccount: Keypair;
+  let quoteAssetPoolAccount: Keypair;
+  let remainingAccounts: AccountMeta[] = [];
+  let instructions: TransactionInstruction[] = [];
+
+  let optionAccount: Keypair;
+  let underlyingAccount: Keypair;
+  let writerTokenAccount: Keypair;
+  let size = new u64(2);
+
+  beforeEach(async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10_000_000_000),
+      "confirmed"
+    );
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        minter.publicKey,
+        10_000_000_000
+      ),
+      "confirmed"
+    );
+    size = new u64(2);
+  });
+
+  const mintOptionsTx = async (
+    opts: {
+      underlyingAssetPoolKey?: PublicKey;
+      remainingAccounts?: AccountMeta[];
+      feeOwner?: PublicKey;
+    } = {}
+  ) => {
+    await program.rpc.mintOptionV2(size, {
+      accounts: {
+        userAuthority: minter.publicKey,
+        underlyingAssetMint: optionMarket?.underlyingAssetMint,
+        underlyingAssetPool:
+          opts.underlyingAssetPoolKey || optionMarket?.underlyingAssetPool,
+        underlyingAssetSrc: underlyingAccount.publicKey,
+        optionMint: optionMarket?.optionMint,
+        mintedOptionDest: optionAccount.publicKey,
+        writerTokenMint: optionMarket?.writerTokenMint,
+        mintedWriterTokenDest: writerTokenAccount.publicKey,
+        optionMarket: optionMarket?.key,
+        feeOwner: opts.feeOwner || FEE_OWNER_KEY,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      },
+      remainingAccounts: opts.remainingAccounts
+        ? opts.remainingAccounts
+        : remainingAccounts,
+      signers: [minter],
+    });
+  };
+
+  describe("proper mint", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        optionToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          size.mul(optionMarket.underlyingAmountPerContract).muln(2).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should mint size OptionTokens", async () => {
+      try {
+        await mintOptionsTx();
+      } catch (err) {
+        console.error((err as Error).toString());
+        throw err;
+      }
+      const mintInfo = await optionToken.getMintInfo();
+      assert.equal(mintInfo.supply.toString(), size.toString());
+    });
+
+    it("should mint size WriterTokens", async () => {
+      try {
+        await mintOptionsTx();
+      } catch (err) {
+        console.error((err as Error).toString());
+        throw err;
+      }
+      const writerToken = new Token(
+        provider.connection,
+        optionMarket.writerTokenMint,
+        TOKEN_PROGRAM_ID,
+        payer
+      );
+      const mintInfo = await writerToken.getMintInfo();
+      assert.equal(mintInfo.supply.toString(), size.toString());
+    });
+
+    it("should transfer the underlying from the minter to the pool and take a fee", async () => {
+      if (!mintFeeKey) {
+        throw new Error("mintFeeKey wasn't set when it should be");
+      }
+      const mintFeeAcctBefore = await underlyingToken.getAccountInfo(
+        mintFeeKey
+      );
+      const underlyingPoolBefore = await underlyingToken.getAccountInfo(
+        optionMarket.underlyingAssetPool
+      );
+      const minterUnderlyingBefore = await underlyingToken.getAccountInfo(
+        underlyingAccount.publicKey
+      );
+      try {
+        await mintOptionsTx();
+      } catch (err) {
+        console.error((err as Error).toString());
+        throw err;
+      }
+      const expectedUnderlyingTransfered = size.mul(
+        underlyingAmountPerContract
+      );
+
+      const underlyingPoolAfter = await underlyingToken.getAccountInfo(
+        optionMarket.underlyingAssetPool
+      );
+      const poolDiff = underlyingPoolAfter.amount.sub(
+        underlyingPoolBefore.amount
+      );
+      const mintFeeAcctAfter = await underlyingToken.getAccountInfo(mintFeeKey);
+      assert.equal(
+        poolDiff.toString(),
+        expectedUnderlyingTransfered.toString()
+      );
+
+      const minterUnderlyingAfter = await underlyingToken.getAccountInfo(
+        underlyingAccount.publicKey
+      );
+      const minterUnderlyingDiff = minterUnderlyingAfter.amount.sub(
+        minterUnderlyingBefore.amount
+      );
+      assert.equal(
+        expectedUnderlyingTransfered.neg().toString(),
+        minterUnderlyingDiff.toString()
+      );
+      const mintFeeAcctDiff = mintFeeAcctAfter.amount.sub(
+        mintFeeAcctBefore.amount
+      );
+      assert.equal(mintFeeAcctDiff.toString(), 0);
+    });
+  });
+
+  describe("OptionMarket expired", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program, {
+        // set expiration to 2 seconds from now
+        expiration: new anchor.BN(new Date().getTime() / 1000 + 2),
+      }));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          optionMarket.underlyingAmountPerContract.muln(2).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should error", async () => {
+      try {
+        await wait(2000);
+        await mintOptionsTx();
+        assert.ok(false);
+      } catch (err) {
+        const errMsg = "OptionMarket is expired, can't mint";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+
+  describe("Underlying pool key differs from option market", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          size.mul(optionMarket.underlyingAmountPerContract.muln(2)).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+      // Create a new token account and set it as the underlyingAssetPoolAccount
+      const { tokenAccount } = await initNewTokenAccount(
+        provider.connection,
+        payer.publicKey,
+        underlyingToken.publicKey,
+        payer
+      );
+      optionMarket.underlyingAssetPool = tokenAccount.publicKey;
+    });
+    it("should error", async () => {
+      try {
+        await mintOptionsTx();
+        assert.ok(false);
+      } catch (err) {
+        const errMsg =
+          "Underlying pool account does not match the value on the OptionMarket";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+
+  describe("OptionToken Mint key differs from option market", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      // Create a new token mint and set it as the optionMintAccount
+      const { mintAccount } = await initNewTokenMint(
+        provider.connection,
+        payer.publicKey,
+        payer
+      );
+      optionMarket.optionMint = mintAccount.publicKey;
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          size.mul(optionMarket.underlyingAmountPerContract.muln(2)).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should error", async () => {
+      try {
+        await mintOptionsTx();
+        assert.ok(false);
+      } catch (err) {
+        const errMsg =
+          "OptionToken mint does not match the value on the OptionMarket";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+
+  describe("WriterToken Mint key differs from option market", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      // Create a new token mint and set it as the optionMintAccount
+      const { mintAccount } = await initNewTokenMint(
+        provider.connection,
+        payer.publicKey,
+        payer
+      );
+      optionMarket.writerTokenMint = mintAccount.publicKey;
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          size.mul(optionMarket.underlyingAmountPerContract.muln(2)).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should error", async () => {
+      try {
+        await mintOptionsTx();
+        assert.ok(false);
+      } catch (err) {
+        const errMsg =
+          "WriterToken mint does not match the value on the OptionMarket";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+
+  describe("Size <= 0", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          optionMarket.underlyingAmountPerContract.muln(2).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+
+      // Set the size to 0 to trigger an error
+      size = new anchor.BN(0);
+    });
+    it("should error", async () => {
+      try {
+        await mintOptionsTx();
+        assert.ok(false);
+      } catch (err) {
+        const errMsg = "The size argument must be > 0";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+  describe("Fee owner is incorrect", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          optionMarket.underlyingAmountPerContract.muln(2).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should error", async () => {
+      try {
+        await mintOptionsTx({
+          feeOwner: new Keypair().publicKey,
+        });
+        assert.ok(false);
+      } catch (err) {
+        const errMsg = "Fee owner does not match the program's fee owner";
+        assert.equal((err as Error).toString(), errMsg);
+      }
+    });
+  });
+  describe("OptionMarket is for NFT", () => {
+    beforeEach(async () => {
+      ({
+        quoteToken,
+        underlyingToken,
+        underlyingAmountPerContract,
+        quoteAmountPerContract,
+        expiration,
+        optionMarketKey,
+        bumpSeed,
+        mintFeeKey,
+        exerciseFeeKey,
+        optionMarket,
+        remainingAccounts,
+        instructions,
+      } = await initSetup(provider, payer, mintAuthority, program, {
+        underlyingAmountPerContract: new anchor.BN("1"),
+      }));
+      await initOptionMarket(
+        program,
+        payer,
+        optionMarket,
+        remainingAccounts,
+        instructions
+      );
+      ({ optionAccount, underlyingAccount, writerTokenAccount } =
+        await createMinter(
+          provider.connection,
+          minter,
+          mintAuthority,
+          underlyingToken,
+          optionMarket.underlyingAmountPerContract.muln(2).toNumber(),
+          optionMarket.optionMint,
+          optionMarket.writerTokenMint,
+          quoteToken
+        ));
+    });
+    it("should transfer enough lamports as required by the fee", async () => {
+      const minterBefore = await provider.connection.getAccountInfo(
+        minter.publicKey
+      );
+      const feeOwnerBefore =
+        (await provider.connection.getAccountInfo(FEE_OWNER_KEY))?.lamports ||
+        0;
+      try {
+        await mintOptionsTx();
+      } catch (err) {
+        console.error((err as Error).toString());
+        throw err;
+      }
+      const minterAfter = await provider.connection.getAccountInfo(
+        minter.publicKey
+      );
+      const feeOwnerAfter =
+        (await provider.connection.getAccountInfo(FEE_OWNER_KEY))?.lamports ||
+        0;
+      if (!minterAfter?.lamports || !minterBefore?.lamports) {
+        throw new Error("minter has no lamports");
+      }
+      const minterDiff = minterAfter?.lamports - minterBefore?.lamports;
+      const feeOwnerDiff = feeOwnerAfter - feeOwnerBefore;
+      assert.equal(-minterDiff, new BN(0));
+      assert.equal(feeOwnerDiff, new BN(0));
+    });
+  });
+});
