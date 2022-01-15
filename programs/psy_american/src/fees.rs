66f98d8a30bbd831a377b5521e78f6b15d96c407
LM <Mahan.Luke@gmail.com>
2022-01-14 21:34:30 -0500

    Typo fix

--- programs/psy_american/src/fees.rs
+++ programs/psy_american/src/fees.rs
@@ -1,5 +1,5 @@
 /// The fee_owner_key will own all of the associated accounts where token fees are paid to.
-/// In the future this should be a program derived address owned by a fully decntralized
+/// In the future this should be a program derived address owned by a fully decentralized
 /// fee sweeping program.
 pub mod fee_owner_key {
   use solana_program::declare_id;
